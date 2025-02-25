f2870240dfad2e0e83f04cc6c05329ff4ce49de1
vkservicesllc <projects@vkservicesllc.com>
2025-02-24 21:07:15 -0500

    fixed the bug in super admin user configurations

--- client/admin/modules/user.mjs
+++ client/admin/modules/user.mjs
@@ -4,40 +4,40 @@ const { id } = formSelectors.user
 const _id = $(`#${id}`).val()
 const categories = $.ajax('/api/assets/company?filter=categories', { async: false, method: 'POST' }).responseJSON
 
-
-$.ajax(`/api/user/${_id}/teams`, {
-    method: 'POST',
-    success(response) {
-        const { data: teams } = response
-        const $teams = {
-            available: $('#available-teams'),
-            applied: $('#user-teams'),
-        }
-
-        const options = { available: '', applied: '' }
-        const optgroups = {}
-        const optgroup = '<optgroup><option value=""></option></optgroup>'
-
-        for (const prop of ['available', 'applied']) {
-            optgroups[prop] = teams[prop].reduce((cat, { _id, name, catId}) => {
-                if (!cat[catId]) cat[catId] = []
-                cat[catId].push({ _id, name })
-    
-                return cat
-            }, {})
-
-            for (const catId in optgroups[prop]) {
-                options[prop] += `<optgroup label="${categories[catId].item[1]}">`
-                for (const team of optgroups[prop][catId]) {
-                    const { _id, name } = team
-                    options[prop] += `<option value="${_id}">${name}</option>`
-                }
-                options[prop] += '</optgroup>'
+if (_id)
+    $.ajax(`/api/user/${_id}/teams`, {
+        method: 'POST',
+        success(response) {
+            const { data: teams } = response
+            const $teams = {
+                available: $('#available-teams'),
+                applied: $('#user-teams'),
             }
-        }
 
-        $teams.available.html(options.available || optgroup)
-        $teams.applied.html(options.applied || optgroup)
-        $('.loader-wrapper').remove()
-    },
-})
+            const options = { available: '', applied: '' }
+            const optgroups = {}
+            const optgroup = '<optgroup><option value=""></option></optgroup>'
+
+            for (const prop of ['available', 'applied']) {
+                optgroups[prop] = teams[prop].reduce((cat, { _id, name, catId}) => {
+                    if (!cat[catId]) cat[catId] = []
+                    cat[catId].push({ _id, name })
+        
+                    return cat
+                }, {})
+
+                for (const catId in optgroups[prop]) {
+                    options[prop] += `<optgroup label="${categories[catId].item[1]}">`
+                    for (const team of optgroups[prop][catId]) {
+                        const { _id, name } = team
+                        options[prop] += `<option value="${_id}">${name}</option>`
+                    }
+                    options[prop] += '</optgroup>'
+                }
+            }
+
+            $teams.available.html(options.available || optgroup)
+            $teams.applied.html(options.applied || optgroup)
+            $('.loader-wrapper').remove()
+        },
+    })
