22c7e4cae46391696e0fd52a9e4334ad53217d96
vkservicesllc <projects@vkservicesllc.com>
2025-04-30 22:31:56 -0400

    finished but did not test the application registration in driver branch

--- client/driver/modules/application/start.mjs
+++ client/driver/modules/application/start.mjs
@@ -2,29 +2,27 @@ import { inputEvent, selectEvent } from '/modules/events/form.mjs'
 import { nameEvent, ssnEvent } from '/modules/events/person.mjs'
 import { telEvent, emailEvent } from '/modules/events/contacts.mjs'
 import { addr1Event, addr2Event, zipEvent, cityEvent } from '/modules/events/address.mjs'
-import { formSelectors } from '/modules/registry/selectors.mjs'
+import selector from '/modules/registry/selectors/driver-application.mjs'
 import { check } from './support.mjs'
 
-const {
-    class: aplClass,
-    firstNameId,
-    middleNameId,
-    lastNameId,
-    suffixId,
-    dobId,
-    ssnId,
-    sexId,
-    phoneId,
-    emailId,
-    addr1Id,
-    addr2Id,
-    zipId,
-    cityId,
-    stateId,
-    addrSinceId,
-    positionId,
-    statusExpId,
-} = formSelectors.driver
+const TS = selector.id.text, SS = selector.id.select
+const firstNameId = TS.firstName
+const middleNameId = TS.middleName
+const lastNameId = TS.lastName
+const suffixId = SS.suffix
+const dobId = TS.dob
+const ssnId = TS.ssn
+const genderId = SS.gender
+const phoneId = TS.phone
+const emailId = TS.email
+const addr1Id = TS.address1
+const addr2Id = TS.address2
+const zipId = TS.addrZip
+const cityId = TS.addrCity
+const stateId = SS.addrState
+const addrSinceId = TS.addrSince
+const positionId = SS.position
+const statusExpId = TS.statusExp
 
 const $card = $('#new-apl-card')
 const $help = {
@@ -34,8 +32,9 @@ const $help = {
     statusExp: $('#status-exp-help'),
     form: $('#form-help'),
 }
-const $addrState = $(`#${stateId}`)
-const $expiration = $(`#${statusExpId}`)
+const $addrState = $(stateId)
+const $status = $('.status-radio')
+const $expiration = $(statusExpId)
 const $submit = $('[type=submit]')
 const $form = $('#apl-start-form')
 
@@ -43,7 +42,7 @@ const dateOpts = { mask: '99/99/9999', placeholder: 'MM/DD/YYYY' }
 
 
 /* Reset Form on Refresh */
-$(`.${aplClass}`).val(null)
+$(selector.class.global).val(null)
 $('.form-check-input').prop('checked', false)
 $expiration.prop('disabled', true)
 
@@ -64,7 +63,7 @@ if (aplStatus === 'started') {
     dobId,
     phoneId,
     emailId,
-    sexId,
+    genderId,
     addr1Id,
     addr2Id,
     zipId,
@@ -73,10 +72,10 @@ if (aplStatus === 'started') {
     addrSinceId,
     positionId,
 ].forEach(id => {
-    const value = sessionStorage.getItem(id)
+    const value = sessionStorage.getItem(id.replace('#', ''))
 
     if (value) {
-        const $el = $(`#${id}`)
+        const $el = $(id)
         const required = $el.prop('required')
 
         $el.val(value)
@@ -108,7 +107,23 @@ $('#confirm').click(() => {
 })
 
 
-$('.status-radio').click(function() {
+$('#confirm-status').click(function() {
+    const disabled = !$(this).is(':checked')
+    $status.prop('disabled', disabled)
+
+    if (disabled) {
+        $status.prop('checked', false)
+        $expiration
+            .val(null)
+            .prop('disabled', true)
+            .removeClass('is-valid is-invalid')
+            .prev().removeClass('input-required')
+            .parent().hide()
+        $help.statusExp.text(null)
+    }
+})
+
+$status.click(function() {
     let disabled = true, action = 'removeClass', parentAction = 'hide'
 
     if ($(this).val() == '2') {
@@ -121,7 +136,7 @@ $('.status-radio').click(function() {
         .val(null)
         .prop('disabled', disabled)
         .removeClass('is-valid is-invalid')
-        .prev()[action]('required')
+        .prev()[action]('input-required')
         .parent()[parentAction]()
     $help.statusExp.text(null)
 })
@@ -153,7 +168,7 @@ nameEvent(lastNameId, { sfxId: suffixId, onInput,
 
 selectEvent(suffixId, { onChange })
 
-selectEvent(sexId, { onChange })
+selectEvent(genderId, { onChange })
 
 ssnEvent(ssnId, { onInput, onChange, onBlur })
 
