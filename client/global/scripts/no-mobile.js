ef687e91ae5b4ef1eefbde15d5346391debe3771
vkservicesllc <projects@vkservicesllc.com>
2025-05-03 22:04:03 -0400

    fixed bug in no-mobile.js

--- client/global/scripts/no-mobile.js
+++ client/global/scripts/no-mobile.js
@@ -2,7 +2,12 @@ $(() => {
     const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i.test(navigator.userAgent)
 
     if (mobile) {
-        alert('Attention: Mobile devices are currently restricted!')
-        window.location.href = $(`#${formSelectors.user.logoutLinkId}`).attr('href')
+        $.ajax('/api/session/current', {
+            method: 'POST',
+            success(response) {
+                alert('Attention: Mobile devices are currently restricted!')
+                window.location.href = response.logoutUrl
+            }
+        })
     }
 })
