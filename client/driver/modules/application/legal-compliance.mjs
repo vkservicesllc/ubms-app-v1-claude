f4e0d697ab47c8ceaa3cb15fdb774cf2294145a9
vkservicesllc <projects@vkservicesllc.com>
2025-05-09 10:17:49 -0400

    added legal-compliance module

---
+++ client/driver/modules/application/legal-compliance.mjs
@@ -0,0 +1,13 @@
+import { inputEvent } from '/modules/events/form.mjs'
+import { check, onInput, onChange, onBlur, onSubmit, onYesNoRadioChange } from './support.mjs'
+import selector from '/modules/registry/selectors/driver-application.mjs'
+
+const RS = selector.id.radio
+const duiId = RS.dui
+const criminalId = RS.criminal
+const criminalExplId = selector.id.text.criminalExpl
+const citationId = RS.citation
+
+const $card = $('#apl-card')
+
+onYesNoRadioChange(criminalId, criminalExplId)
