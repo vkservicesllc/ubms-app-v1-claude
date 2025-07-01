b892ea2e3f9b963fca7fcc2b5d21446252821226
vkservicesllc <projects@vkservicesllc.com>
2025-07-01 16:11:05 -0400

    added console.log to test smth

--- client/carrier/modules/drivers/applications.mjs
+++ client/carrier/modules/drivers/applications.mjs
@@ -76,15 +76,16 @@ const table = $('#driver-apl-table').DataTable({
 
                 if (row.marital === 'm') {
                     let { sex, benefRelation, benefOtherRel } = row
-                    benefRelation = benefRelation.toLowerCase().trim()
-                    if (benefOtherRel) benefOtherRel.toLowerCase().trim()
+console.log({ sex, benefRelation, benefOtherRel })
+                    // benefRelation = benefRelation.toLowerCase().trim()
+                    // if (benefOtherRel) benefOtherRel.toLowerCase().trim()
 
-                    switch (true) {
-                        case sex === 0 && (benefRelation === 'wife' || benefOtherRel === 'wife'):
-                        case sex === 1 && (benefRelation === 'husband' || benefOtherRel === 'husband'):
-                            data += `<span title="Logical Error: Incorrect Gender"><i class="ui red text exclamation triangle icon"></i></span>`
-                            break
-                    }
+                    // switch (true) {
+                    //     case sex === 0 && (benefRelation === 'wife' || benefOtherRel === 'wife'):
+                    //     case sex === 1 && (benefRelation === 'husband' || benefOtherRel === 'husband'):
+                    //         data += `<span title="Logical Error: Incorrect Gender"><i class="ui red text exclamation triangle icon"></i></span>`
+                    //         break
+                    // }
                 }
 
                 return data
