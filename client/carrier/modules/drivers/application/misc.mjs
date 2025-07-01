aa9106552221982e6a2d2cd2e64f23d225816b92
vkservicesllc <projects@vkservicesllc.com>
2025-07-01 18:07:50 -0400

    added emergency contact to application in ui

---
+++ client/carrier/modules/drivers/application/misc.mjs
@@ -0,0 +1,17 @@
+import { inputEvent } from '/modules/events/form.mjs'
+import { nameEvent } from '/modules/events/person.mjs'
+import { telEvent } from '/modules/events/contacts.mjs'
+import selector from '/modules/registry/selectors/driver-application.mjs'
+import application from './hub.mjs'
+
+
+(() => {
+    if (!application || !Object.keys(application).length) return
+
+    const { phone, name, relation } = application.emergency
+    const TS = selector.id.text
+
+    telEvent(TS.emergPhone, { value: phone })
+    nameEvent(TS.emergName, { value: name })
+    inputEvent(TS.emergRelation, { strip: true, word: true, capitalize: 'first', value: relation })
+})()
