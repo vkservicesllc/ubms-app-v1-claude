1a10a06a8f9e0703fd2ebb2488b57d4d514fe744
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2025-11-20 16:32:51 -0500

    finished fetch in class instance

--- server/tools/utils/class.mjs
+++ server/tools/utils/class.mjs
@@ -6,10 +6,15 @@ const mysql = require('./mysql')
 export const _instance = {
 
 
-    add: async (inst, cls, target, bodyOrIds, bodyCB = null) => {
+    redFields: ['createdBy', 'createdAt', 'createdIn', 'updateLog', 'deletedBy', 'deletedAt', 'deletedIn'],
+    logFields: ['createdBy', 'createdAt', 'updateLog'],
+
+
+    add: async (inst, Cls, target, bodyOrIds, bodyCB = null) => {
         const { enforceUser = true } = inst.config
         const { user: sessionUser } = inst.session || {}
-        if (enforceUser && !sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [ADD]: Session user not supplied`)
+        if (enforceUser && !sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [ADD]: Session user not supplied`)
+        if (target || target === 'main') throw new Error(`${Cls.name} Constructor Method Error [ADD]: Target not supplied`)
 
         const createdBy = sessionUser?.id || null
         const jx = target.slice(0, 3) === 'jx.'
@@ -17,10 +22,10 @@ export const _instance = {
 
         if (jx) {
             const { jxTargets, idProp: refIdProp } = inst.config
-            if (!jxTargets) throw new Error(`${cls.name} Constructor Method Error [ADD]: Junction targets not found`)
+            if (!jxTargets) throw new Error(`${Cls.name} Constructor Method Error [ADD]: Junction targets not found`)
 
             const ids = bodyOrIds || []
-            if (!Array.isArray(ids)) throw new Error(`${cls.name} Constructor Method Error [ADD]: Invalid ids supplied`)
+            if (!Array.isArray(ids)) throw new Error(`${Cls.name} Constructor Method Error [ADD]: Invalid ids supplied`)
 
             const [ jxQuery, idProp, Src ] = jxTargets[target]
             const data = []
@@ -31,65 +36,77 @@ export const _instance = {
             const [ result ] = await mysql.execute(jxQuery.insert(data))
 
             return result.affectedRows > 0 // Boolean
-        } else if (target) {
-            let body = bodyOrIds || {}
-            const { query, logLocation = false } = inst.config
-
-            body = processData(body)
-            if (typeof bodyCB === 'function') body = bodyCB(body)
-            body.createdBy = createdBy
-
-            if (logLocation) {
-                const { branch, siteId } = inst.session
-                const createdIn = { branch }
-                if (siteId) createdIn.siteId = siteId
-
-                body.createdIn = JSON.stringify(createdIn)
-            }
-
-            const [ result ] = await mysql.execute(query[target].insert(body))
-
-            return result.affectedRows > 0 // Boolean
         }
+
+        let body = bodyOrIds || {}
+        const { query, logLocation = false } = inst.config
+
+        body = processData(body)
+        if (typeof bodyCB === 'function') body = bodyCB(body)
+        body.createdBy = createdBy
+
+        if (logLocation) {
+            const { branch, siteId } = inst.session
+            const createdIn = { branch }
+            if (siteId) createdIn.siteId = siteId
+
+            body.createdIn = JSON.stringify(createdIn)
+        }
+
+        const [ result ] = await mysql.execute(query[target].insert(body))
+
+        return result.affectedRows > 0 // Boolean
     },
 
 
-    fetch: async (inst, cls, target, since) => {
+    fetch: async (inst, Cls, target, { hideRawId = false, hideSensitive = true, idsOnly = false, sorts = null, since } = {}) => {
         const { enforceUser = true } = inst.config
         const { user: sessionUser } = inst.session || {}
-        if (enforceUser && !sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [FETCH]: Session user not supplied`)
+        if (enforceUser && !sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [FETCH]: Session user not supplied`)
+        if (target || target === 'main') throw new Error(`${Cls.name} Constructor Method Error [FETCH]: Target not supplied`)
 
+        const { idProp } = inst.config
         const jx = target.slice(0, 3) === 'jx.'
         if (jx) target = target.slice(3)
 
         if (jx) {
             const { jxTargets } = inst.config
-            if (!jxTargets) throw new Error(`${cls.name} Constructor Method Error [FETCH]: Junction targets not found`)
+            if (!jxTargets) throw new Error(`${Cls.name} Constructor Method Error [FETCH]: Junction targets not found`)
 
-            const [ jxQuery, idProp, Src, defSorts ] = jxTargets[target]
+            const ids = []
+            const [ jxQuery, jxIdProp, Src, defSorts ] = jxTargets[target]
+            if (!sorts) sorts = defSorts
 
-            //! not finished...
+            const [ rows ] = await mysql.execute(jxQuery.select(jxIdProp, { match: { [idProp]: inst.id } }))
+            rows.map(row => ids.push(row[idProp]))
+
+            return idsOnly ? ids : await Src.fetch(inst.session, { ids }, { hideRawId, hideSensitive, sorts })
         }
 
-        //! not finished...
+        const { query, redFields } = inst.config
+        if (!redFields[target]) redFields[target] = this.redFields
+
+        const [ rows ] = await mysql.execute(query[target].select('*', { [idProp]: inst.id, since }))
+        rows.map(row => { redFields.map(redField => delete row[redField]) })
+
+        return since ? rows[0] : rows
     },
 
 
-    update: async (inst, cls, targetOrBody, body) => {
+    update: async (inst, Cls, targetOrBody, body) => {
         const { enforceUser = true } = inst.config
         const { user: sessionUser } = inst.session || {}
-        if (enforceUser && !sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [UPDATE]: Session user not supplied`)
+        if (enforceUser && !sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [UPDATE]: Session user not supplied`)
 
         //! not finished...
     },
 
 
-    delete: async (inst, cls, targetOrCB = null, sinceOrIds) => {
+    delete: async (inst, Cls, targetOrCB = null, sinceOrIds) => {
         const { user: sessionUser } = inst.session || {}
-        if (!sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [DELETE]: Session user not supplied`)
+        if (!sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [DELETE]: Session user not supplied`)
 
         let target = typeof targetOrCB === 'string' ? targetOrCB : null
-        const handle = typeof targetOrCB === 'function' ? targetOrCB : null
         if (target === 'main') target = null
 
         const { idProp } = inst.config
@@ -98,10 +115,10 @@ export const _instance = {
 
         if (jx) {
             const { jxTargets } = inst.config
-            if (!jxTargets) throw new Error(`${cls.name} Constructor Method Error [DELETE]: Junction targets not found`)
+            if (!jxTargets) throw new Error(`${Cls.name} Constructor Method Error [DELETE]: Junction targets not found`)
 
             const ids = sinceOrIds || []
-            if (!Array.isArray(ids)) throw new Error(`${cls.name} Constructor Method Error [DELETE]: Invalid ids supplied`)
+            if (!Array.isArray(ids)) throw new Error(`${Cls.name} Constructor Method Error [DELETE]: Invalid ids supplied`)
 
             const [ jxQuery, jxIdProp ] = jxTargets[target]
             const [ result ] = await mysql.execute(jxQuery.delete({ [idProp]: inst.id, [jxIdProp]: ids }))
@@ -114,38 +131,41 @@ export const _instance = {
             const [ result ] = await mysql.execute(query[target].delete({ [idProp]: inst.id, since }))
 
             return result.affectedRows > 0 // Boolean
-        } else {
-            if (handle) return handle(inst, cls)
-
-            const { query, logDeleted = true, logFile } = inst.config
-            const { id } = inst
-            const log = logDeleted && logFile ? await inst.log() : null
-
-            const [ result ] = await mysql.execute(query.main.delete({ id }))
-            if (!result.affectedRows) return false
-
-            if (log) {
-                for (const prop in log) inst[prop] = log[prop]
-
-                await logDeletion(inst.session, logFile, inst, { id })
-            }
-
-            return true
         }
+
+        const handle = typeof targetOrCB === 'function' ? targetOrCB : null
+        if (handle) return handle(inst, Cls)
+
+        const { query, logDeleted = true, logFile } = inst.config
+        const { id } = inst
+        const log = logDeleted && logFile ? await inst.log() : null
+
+        const [ result ] = await mysql.execute(query.main.delete({ id }))
+        if (!result.affectedRows) return false
+
+        if (log) {
+            for (const prop in log) inst[prop] = log[prop]
+
+            await logDeletion(inst.session, logFile, inst, { id })
+        }
+
+        return true
     },
 
 
-    history: async (inst, cls, target) => {
+    history: async (inst, Cls, target) => {
         const { user: sessionUser } = inst.session || {}
-        if (!sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [HISTORY]: Session user not supplied`)
+        if (!sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [HISTORY]: Session user not supplied`)
 
         //! not finished...
     },
 
 
-    log: async (inst, cls, field = null, { target = 'main', fields = ['createdBy', 'createdAt', 'updateLog'] }) => {
+    log: async (inst, Cls, field = null, { target = 'main', fields }) => {
         const { user: sessionUser } = inst.session || {}
-        if (!sessionUser?.id) throw new Error(`${cls.name} Constructor Method Error [LOG]: Session user not supplied`)
+        if (!sessionUser?.id) throw new Error(`${Cls.name} Constructor Method Error [LOG]: Session user not supplied`)
+
+        fields = fields ?? this.logFields
 
         const { query } = inst.config
         const log = (await mysql.execute(query[target].select(fields, { match: { id: inst.id } })))[0][0]
