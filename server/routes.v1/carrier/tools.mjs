229aa1ba86a3f7a21029c18dd8d58a2d68aa9de7
vkservicesllc <12345678+yourusername@users.noreply.github.com>
2026-01-22 22:07:07 -0500

    removed redundant old files

--- server/routes.v1/carrier/tools.mjs
+++
@@ -1,7 +0,0 @@
-export const navBuilder = {
-    simple: items => {
-        let html = ''
-        items.forEach(item => html += `\n\t\t<a class="${item[2] ? 'active ' : ''}item" href="${item[0]}">${item[1]}</a>`)
-        return html
-    },
-}
--- server/routes.v1/driver/upload.mjs
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-/* Tools */
-import uploader from '../../tools/utils/multer.mjs'
-
-
-
-export default router
